d7712656b9d1f138b15cf2aa3441495afcd6fbf5
Olga Khaidoukova <khaidoukova@inbox.ru>
2023-05-23 14:23:48 +0500

    homework-2

--- src/item.py
+++ src/item.py
@@ -1,10 +1,13 @@
+import csv
+from csv import DictReader
+
+
 class Item:
     """
     Класс для представления товара в магазине.
     """
     pay_rate = 1.0
     all = []
-
 
     def __init__(self, name: str, price: float, quantity: int):
         """
@@ -14,22 +17,42 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         Item.all.append(self)
 
-    def calculate_total_price(self):
-        """
-        Рассчитывает общую стоимость конкретного товара в магазине.
+    @property
+    def name(self):
+        return self.__name
 
-        :return: Общая стоимость товара.
-        """
-        return self.price*self.quantity
+    @name.setter
+    def name(self, new_name: str):
+        try:
+            if len(new_name) <= 10:
+                self.__name = new_name
+        except:
+            raise AttributeError("Длина названия больше 10 символов")
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        """Инициализируем экземпляры класса данными из файла"""
+        cls.all.clear()
+        with open('../src/items.csv', newline="") as csv_file:
+            reader = csv.DictReader(csv_file)
+            for line in reader:
+                cls(line["name"], line["price"], line["quantity"])
+
+
+    @staticmethod
+    def string_to_number(line):
+        numb = int(float(line))
+        return numb
+
+    def calculate_total_price(self):
+
+        return self.price * self.quantity
 
     def apply_discount(self):
-        """
-        Применяет установленную скидку для конкретного товара.
-        """
-        self.price *= self.pay_rate
 
+        self.price *= self.pay_rate
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,6 +1,7 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
 import pytest
 from src.item import Item
+import csv
 
 
 def test_calculate_total_price():
@@ -14,3 +15,20 @@ def test_apply_discount():
     item1.apply_discount()
     expected = 8000
     assert item1.price == expected
+
+def test_instantiate_from_csv():
+    Item.all = []
+    with open("../tests/items.csv", newline="") as file:
+        reader = csv.DictReader(file)
+        for line in reader:
+            Item.all.append(line)
+    assert Item.all[0] == {'name': 'Smartphone', 'price': '100', 'quantity': '1'}
+    assert len(Item.all) == 5
+
+
+def test_string_to_number():
+    assert Item.string_to_number('5') == 5
+    assert Item.string_to_number('5.0') == 5
+    assert Item.string_to_number('5.5') == 5
+
+
